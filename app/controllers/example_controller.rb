ef58cc635b2cb6528c332145f01dda6a6095f21d
Rola1993 <rola.uiuc@gmail.com>
2019-01-15 09:19:44 -0800

    test

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -20,7 +20,7 @@ class ExampleController < ActionController::Base
     http.verify_depth = 5
 
     new_request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-    new_request.body = "#{request.body}".to_json
+    new_request.body = "#{request.body.read}".to_json
     response = http.request(new_request)
     render plain: "Response #{response.code} #{response.message}: #{response.body}"
   end
