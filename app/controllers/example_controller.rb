7a3c6a699e3d92e33e5b3d5551bd294e00194229
Rola1993 <rola.uiuc@gmail.com>
2019-01-15 07:38:01 -0800

    test outbound

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -20,7 +20,7 @@ class ExampleController < ActionController::Base
 
     new_request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
     new_request.body = "#{request.body.read}".to_json
-    response = http.request(new_equest)
+    response = http.request(new_request)
     render plain: "Response #{response.code} #{response.message}: #{response.body}"
   end
 end
