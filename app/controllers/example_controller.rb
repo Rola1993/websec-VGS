0489f1669196ea5f2d6894fd366721cee6b7c74c
Rola1993 <rola.uiuc@gmail.com>
2019-01-15 07:16:31 -0800

    test for outbound

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,5 +1,7 @@
 require 'uri'
 require 'net/http'
+require 'net/https'
+require 'json'
 
 class ExampleController < ActionController::Base
 
@@ -8,17 +10,17 @@ class ExampleController < ActionController::Base
   end
 
   def curl_send
-    url = URI("https://echo.apps.verygood.systems/post")
+  proxy = URI.parse('https://<USERNAME>:<PASSWORD>@tnt5jvcsgxf.sandbox.verygoodproxy.com:8080')
+  uri = URI.parse('https://echo.apps.verygood.systems/post')
+  http = Net::HTTP.new(uri.host, uri.port, proxy.host, proxy.port, proxy.user, proxy.password)
+  http.use_ssl = true
+  http.ca_file = '../../cert.pem'
+  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  http.verify_depth = 5
 
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true if url.scheme == 'https'   
-
-    new_request = Net::HTTP::Post.new(url, initheader = {'Content-Type' =>'application/json'})
-    new_request["cache-control"] = 'no-cache'
-    new_request["Postman-Token"] = '2bc99c28-a586-41c7-86d0-5d3c427e5d39'
-    new_request.body = "#{request.body.read}"
-
-    response = http.request(new_request)
-    render plain: "#{response.read_body}"
+  request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+  request.body = {secret: 'ALIAS'}.to_json
+  response = http.request(request)
+  render plain: "Response #{response.code} #{response.message}: #{response.body}"
   end
 end
