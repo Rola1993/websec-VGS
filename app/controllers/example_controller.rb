a5ee5dc4edd16901054bf78d79aa9c7308196eaa
Rola1993 <rola.uiuc@gmail.com>
2019-01-15 07:34:34 -0800

    test outbound

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -10,17 +10,17 @@ class ExampleController < ActionController::Base
   end
 
   def curl_send
-  proxy = URI.parse('https://<USERNAME>:<PASSWORD>@tnt5jvcsgxf.sandbox.verygoodproxy.com:8080')
-  uri = URI.parse('https://echo.apps.verygood.systems/post')
-  http = Net::HTTP.new(uri.host, uri.port, proxy.host, proxy.port, proxy.user, proxy.password)
-  http.use_ssl = true
-  http.ca_file = '/cert.pem'
-  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-  http.verify_depth = 5
+    proxy = URI('https://<USERNAME>:<PASSWORD>@tnt5jvcsgxf.sandbox.verygoodproxy.com:8080')
+    uri = URI('https://echo.apps.verygood.systems/post')
+    http = Net::HTTP.new(uri.host, uri.port, proxy.host, proxy.port, proxy.user, proxy.password)
+    http.use_ssl = true
+    http.ca_file = '/cert.pem'
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    http.verify_depth = 5
 
-  new_request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-  new_request.body = "#{request.body.read}".to_json
-  response = http.request(new_equest)
-  render plain: "Response #{response.code} #{response.message}: #{response.body}"
+    new_request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+    new_request.body = "#{request.body.read}".to_json
+    response = http.request(new_equest)
+    render plain: "Response #{response.code} #{response.message}: #{response.body}"
   end
 end
